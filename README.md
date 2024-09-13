264936df1edf503bb2ecf6283ea1b413bfcac3be
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-14 02:31:05 +0530

    converted to pulumi git repo

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 This is a Planton Cloud Pulumi Module to create [AWS VPC](https://docs.aws.amazon.com/vpc/latest/userguide/what-is-amazon-vpc.html).
 
+```shell
+pulumi stack select --stack <pulumi-org-name>/<pulumi-stack-name>
+```
+
+```shell
+export STACK_INPUT_FILE_PATH=<path-to-stack-input-yaml-file>
+pulumi up
+```
--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -2,65 +2,12 @@ package outputs
 
 import (
 	"fmt"
-	"github.com/plantoncloud/aws-vpc-pulumi-module/pkg/localz"
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/awsvpc"
-	"github.com/plantoncloud/stack-job-runner-golang-sdk/pkg/automationapi/autoapistackoutput"
-	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 )
 
 const (
 	VpcId             = "vpc-id"
 	InternetGatewayId = "internet-gateway-id"
 )
-
-func PulumiOutputsToStackOutputsConverter(pulumiOutputs auto.OutputMap, stackInput *awsvpc.AwsVpcStackInput) *awsvpc.AwsVpcStackOutputs {
-	privateAzSubnetMap := localz.GetPrivateAzSubnetMap(stackInput.ApiResource)
-	sortedPrivateAzKeys := localz.GetSortedAzKeys(privateAzSubnetMap)
-	privateSubnetOutputs := make([]*awsvpc.AwsVpcSubnetStackOutputs, 0)
-
-	for _, availabilityZone := range sortedPrivateAzKeys {
-		azSubnetMap := privateAzSubnetMap[localz.AvailabilityZone(availabilityZone)]
-		sortedSubnetNames := localz.GetSortedSubnetNameKeys(azSubnetMap)
-		for _, subnetName := range sortedSubnetNames {
-			subnetStackOutputs := &awsvpc.AwsVpcSubnetStackOutputs{
-				Name: subnetName,
-				Id:   autoapistackoutput.GetVal(pulumiOutputs, SubnetIdOutputKey(subnetName)),
-				Cidr: autoapistackoutput.GetVal(pulumiOutputs, SubnetCidrOutputKey(subnetName)),
-			}
-			if stackInput.ApiResource.Spec.IsNatGatewayEnabled {
-				subnetStackOutputs.NatGateway = &awsvpc.AwsVpcNatGatewayStackOutputs{
-					Id:        autoapistackoutput.GetVal(pulumiOutputs, NatGatewayIdOutputKey(subnetName)),
-					PrivateIp: autoapistackoutput.GetVal(pulumiOutputs, NatGatewayPrivateIpOutputKey(subnetName)),
-					PublicIp:  autoapistackoutput.GetVal(pulumiOutputs, NatGatewayPublicIpOutputKey(subnetName)),
-				}
-			}
-			privateSubnetOutputs = append(privateSubnetOutputs, subnetStackOutputs)
-		}
-	}
-
-	publicAzSubnetMap := localz.GetPublicAzSubnetMap(stackInput.ApiResource)
-	sortedPublicAzKeys := localz.GetSortedAzKeys(publicAzSubnetMap)
-	publicSubnetOutputs := make([]*awsvpc.AwsVpcSubnetStackOutputs, 0)
-	for _, availabilityZone := range sortedPublicAzKeys {
-		azSubnetMap := publicAzSubnetMap[localz.AvailabilityZone(availabilityZone)]
-		sortedSubnetNames := localz.GetSortedSubnetNameKeys(azSubnetMap)
-		for _, subnetName := range sortedSubnetNames {
-			subnetStackOutputs := &awsvpc.AwsVpcSubnetStackOutputs{
-				Name: subnetName,
-				Id:   autoapistackoutput.GetVal(pulumiOutputs, SubnetIdOutputKey(subnetName)),
-				Cidr: autoapistackoutput.GetVal(pulumiOutputs, SubnetCidrOutputKey(subnetName)),
-			}
-			publicSubnetOutputs = append(publicSubnetOutputs, subnetStackOutputs)
-		}
-	}
-
-	return &awsvpc.AwsVpcStackOutputs{
-		VpcId:             autoapistackoutput.GetVal(pulumiOutputs, VpcId),
-		InternetGatewayId: autoapistackoutput.GetVal(pulumiOutputs, InternetGatewayId),
-		PrivateSubnets:    privateSubnetOutputs,
-		PublicSubnets:     publicSubnetOutputs,
-	}
-}
 
 func SubnetIdOutputKey(subnetName string) string {
 	return fmt.Sprintf("%s-id", subnetName)
